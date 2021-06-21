d213240a6f8401e84fdca5ff09f8a1951f6f0464
David Amateau <damateau@gmail.com>
2021-06-20 23:03:30 -0400

    creating piece classes

---
+++ src/main/java/com/game/MyChessGame/models/pieces/King.java
@@ -0,0 +1,4 @@
+package com.game.MyChessGame.models.pieces;
+
+public class King extends Piece{
+}
---
+++ src/main/java/com/game/MyChessGame/models/pieces/Pawn.java
@@ -0,0 +1,4 @@
+package com.game.MyChessGame.models.pieces;
+
+public class Pawn extends Piece{
+}
