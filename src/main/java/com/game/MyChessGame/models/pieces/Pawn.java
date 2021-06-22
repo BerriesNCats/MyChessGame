f15d54c9a193dc5e3ee9397347c0eb6770b149c3
David Amateau <damateau@gmail.com>
2021-06-21 21:22:52 -0400

    sketching out pawn class

--- src/main/java/com/game/MyChessGame/models/pieces/Pawn.java
+++ src/main/java/com/game/MyChessGame/models/pieces/Pawn.java
@@ -2,15 +2,22 @@ package com.game.MyChessGame.models.pieces;
 
 import com.game.MyChessGame.models.Alliance;
 import com.game.MyChessGame.models.board.Board;
+import com.game.MyChessGame.models.board.BoardUtils;
+import com.game.MyChessGame.models.board.MajorPieceMove;
 import com.game.MyChessGame.models.board.Move;
+import com.google.common.collect.ImmutableList;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 public class Pawn extends Piece{
 
+    private final static int[] CANDIDATE_MOVE_COORDINATES = {8};
 
     public Pawn(int piecePosition, Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
@@ -18,6 +25,22 @@ public class Pawn extends Piece{
 
     @Override
     public Collection<Move> calculateLegalMoves(Board board) {
-        return null;
+
+        final List<Move> legalMoves = new ArrayList<>();
+
+        for(final int currentCandidateDestinationOffset : Pawn.CANDIDATE_MOVE_COORDINATES) {
+            int candidateDestinationCoordinate = this.piecePosition +
+                    (this.getPieceAlliance().getDirection() * currentCandidateDestinationOffset);
+            if(!BoardUtils.isValidTileCoordinate(currentCandidateDestinationOffset)) {
+                continue;
+            }
+            if(currentCandidateDestinationOffset == 8 && board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
+                // TODO FIX
+                legalMoves.add(new MajorPieceMove(board, this, candidateDestinationCoordinate));
+            }
+
+        }
+
+        return ImmutableList.copyOf(legalMoves);
     }
 }
