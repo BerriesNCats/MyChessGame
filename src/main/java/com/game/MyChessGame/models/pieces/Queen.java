c64837927062fed85b6cba73f2faa52d5dffa06e
David Amateau <damateau@gmail.com>
2021-06-21 21:11:04 -0400

    updating Queen class

--- src/main/java/com/game/MyChessGame/models/pieces/Queen.java
+++ src/main/java/com/game/MyChessGame/models/pieces/Queen.java
@@ -1,22 +1,64 @@
 package com.game.MyChessGame.models.pieces;
 
 import com.game.MyChessGame.models.Alliance;
-import com.game.MyChessGame.models.board.Board;
-import com.game.MyChessGame.models.board.Move;
+import com.game.MyChessGame.models.board.*;
+import com.google.common.collect.ImmutableList;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 public class Queen extends Piece{
+
+    private final static int[] CANDIDATE_MOVE_COORDINATES = {-9, -8, -7, -1, 1, 7, 8, 9};
 
     public Queen(int piecePosition, Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
     }
 
     @Override
-    public Collection<Move> calculateLegalMoves(Board board) {
-        return null;
+    public Collection<Move> calculateLegalMoves(final Board board) {
+        final List<Move> legalMoves = new ArrayList<>();
+
+        for(final int currentCandidateDestinationOffset : Queen.CANDIDATE_MOVE_COORDINATES) {
+            int candidateDestinationCoordinate = this.piecePosition;
+            while(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+                if(isFirstColumnExclusion(candidateDestinationCoordinate, currentCandidateDestinationOffset) ||
+                        isEighthColumnExclusion(candidateDestinationCoordinate, currentCandidateDestinationOffset)) {
+                    break;
+                }
+                candidateDestinationCoordinate += currentCandidateDestinationOffset;
+                if(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+                    final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+                    if(!candidateDestinationTile.isTileOccupied()) {
+                        legalMoves.add(new MajorPieceMove(board, this, candidateDestinationCoordinate));
+                    } else {
+                        // If a Queen is blocked by any piece it cannot consider pieces past it
+                        final Piece pieceAtDestination  = candidateDestinationTile.getPiece();
+                        final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
+                        if (this.pieceAlliance != pieceAlliance) {
+                            legalMoves.add(new AttackMove
+                                    (board, this, candidateDestinationCoordinate, pieceAtDestination));
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        return ImmutableList.copyOf(legalMoves);
+    }
+
+    private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset) {
+        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -1 || candidateOffset == -9 ||
+                candidateOffset == 7);
+    }
+
+    private static boolean isEighthColumnExclusion(final int currentPosition, final int candidateOffset) {
+        return BoardUtils.EIGHTH_COLUMN[currentPosition] && (candidateOffset == -7 || candidateOffset == 1 ||
+                candidateOffset == 9);
     }
 }
